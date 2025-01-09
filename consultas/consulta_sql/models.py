e271e871b3b858b90aedb0b3532601a5a22c4c66
blandskron <bastianlandskronfreelancer@gmail.com>
2025-01-09 20:27:32 -0300

    update

---
+++ consultas/consulta_sql/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Factura(models.Model):
+    cliente_id = models.IntegerField()
+    importe = models.DecimalField(max_digits=10, decimal_places=2)
+    pagada = models.BooleanField(default=False)
+    
